47fa404da8c2b4748b49ff055964167328ceaca7
Siddhesh Rane <siddheshrane24@gmail.com>
2018-03-20 20:08:55 +0530

    ui iteractions

--- frontend/examples/bidders.js
+++ frontend/examples/bidders.js
@@ -5,11 +5,13 @@ Vue.component('existing-tenders', {
       existingTenders:[
         {
            name:'sid',
+           bidSubmissionClosingDate:'34/23/45',
            closingDate:'34/10/2',
            address:'sdsd2323'
         },
         {
            name:'sid',
+           bidSubmissionClosingDate:'34/23/45',
            closingDate:'34/10/2',
            address:'dsd238934'
         }
@@ -22,6 +24,8 @@ Vue.component('existing-tenders', {
         var tender_address = event.srcElement.id;
         console.log(tender_address);
         this.$parent.currentView = 'place-bid';
+
+        // pass it
       }
     }
   
@@ -31,8 +35,26 @@ Vue.component('place-bid', {
   template: '#place-bid',
   data:function(){
     return{
-      name:'lol',
-        
+       name:'highway',
+      id:'223',
+      coverCount:21,
+      milestonesCount:5,
+      bidSubmissionClosingDate:'22/13/11',
+      bidOpeningDate:'23/1/34',
+      clauses:[{
+        name:'LOL'
+      },{
+        name:'sdj',
+      }],
+      constraints:[{
+        name:'sdsd'
+      },{
+        name:'sdsd22222'
+      }],
+      milestones:[{
+        name:'lol',
+        days:23
+      }]     
     }
   },
   mounted(){
@@ -68,8 +90,23 @@ Vue.component('placed-bids', {
 Vue.component('active-contracts', {
   template: '#active-contracts',
   methods: {
-    activeContractsDetails: function(){
+    activeContractsDetails: function(event){
+      event.preventDefault();
+      var contract_address = event.srcElement.id;
+      console.log(contract_address);
+      // pass this to active contract details
       this.$parent.currentView = 'active-contracts-details';
+    }
+  },
+  data:function(){
+    return{
+      contracts:[{
+        name:'sdsd',
+        expectedCompletionDate:'23/134/56',
+        status:'pending',
+        address:'23skjbf34'
+      }
+     ]
     }
   }
 })
@@ -82,8 +119,42 @@ Vue.component('active-contracts-details', {
     },
     viewDocs: function(){
       this.$parent.currentView = 'view-submitted-docs';
+    },
+    markMilestoneAsCompleted:function(event){
+        event.preventDefault(); //TBD
+        var milestoneIndex = event.srcElement.id;
+        console.log(milestoneIndex);
+    },
+    withdrawMoney:function(event){
+      event.preventDefault(); //TBD
+        var milestoneIndex = event.srcElement.id;
+        console.log(milestoneIndex);
     }
-
+  },
+  data:function(){
+      return{
+      name:'highway',
+      id:'223',
+      coverCount:21,
+      milestonesCount:5,
+      bidSubmissionClosingDate:'22/13/11',
+      bidOpeningDate:'23/1/34',
+      contractStartingDate:'24/45/45',
+      verifierAddress:'23efks45',
+      clauses:[{
+        name:'LOL',
+        amount:5000
+      },{
+        name:'sdj',
+        amount:51120
+      }],
+      milestones:[{
+        name:'lol',
+        index:0,
+        deadline:'23/10/15',
+        status:'pending'
+      }]
+    }
   }
 })
 
--- frontend/examples/normal.js
+++ frontend/examples/normal.js
@@ -2,15 +2,23 @@ Vue.component('existing-tenders', {
   template: '#existing-tenders',
   data: function(){
     return {
-      existingTenders: []      
+      existingTenders: [{
+        address:'sdsd244',
+        name:'sdsd',
+        closingDate:'23/12/34',
+        bidCount:23
+      }]      
     }
   },
   mounted(){
     this.existingTenders = getExistingTenders();
   },
   methods: {
-    tenderInfo : function(){
+    tenderInfo : function(event){
       this.$parent.currentView = 'tender-info';
+      event.preventDefault();
+      var tender_address = event.srcElement.id;
+      console.log(tender_address);
     }
   }
 })
@@ -23,25 +31,98 @@ Vue.component('ongoing-contracts', {
           {
             name: 'ghar ka kaam',
             completionDate: '12/2/45',
-            status: 'ho gaya'
+            status: 'ho gaya',
+            address:'2jksnf2434'
           }
-      ]
+      ],
+      name:'highway',
+      id:'223',
+      coverCount:21,
+      milestoneCount:5,
+      bidSubmissionClosingDate:'22/13/11',
+      bidOpeningDate:'23/1/34',
+      contractStartingDate:'24/45/45',
+      verifierAddress:'23efks45',
+      contractorAddress:'244jksbfu45',
+      clauses:[{
+        name:'LOL'
+      },{
+        name:'sdj'
+      }],
+      milestones:[{
+        name:'lol',
+        noOfDays:23
+      }]
+
     }
   },
   methods: {
-    ongoingContractDetails : function(){
+    ongoingContractDetails : function(event){
       this.$parent.currentView = 'cm-ongoing-contract-details';
+       event.preventDefault();
+      var contract_address = event.srcElement.id;
+      console.log(contract_address);
     }
   }
 })
 
 Vue.component('tender-info', {
   template: '#tender-info',
-  
+  data:function(){
+    return{
+      name:'highway',
+      id:'223',
+      coverCount:21,
+      milestoneCount:5,
+      bidSubmissionClosingDate:'22/13/11',
+      bidOpeningDate:'23/1/34',
+      contractStartingDate:'24/45/45',
+      verifierAddress:'23efks45',
+      contractorAddress:'244jksbfu45',
+      clauses:[{
+        name:'LOL'
+      },{
+        name:'sdj'
+      }],
+      milestones:[{
+        name:'lol',
+        noOfDays:23
+      }]
+    }
+  },
+  mounted(){
+    // get tender address
+    //call
+  }
 })
 
 Vue.component('cm-ongoing-contract-details', {
   template: '#cm-ongoing-contract-details',
+  data:function(){
+    return{
+      name:'highway',
+      id:'223',
+      coverCount:21,
+      milestoneCount:5,
+      bidSubmissionClosingDate:'22/13/11',
+      bidOpeningDate:'23/1/34',
+      contractStartingDate:'24/45/45',
+      verifierAddress:'23efks45',
+      contractorAddress:'244jksbfu45',
+      clauses:[{
+        name:'LOL',
+        amount:5000
+      },{
+        name:'sdj',
+        amount:65451
+      }],
+      milestones:[{
+        name:'lol',
+        deadline:'22/10/10',
+        status:'completed'
+      }]
+    }
+  },
   methods: {
     viewDocs: function(){
       this.$parent.currentView = 'view-submitted-docs';
